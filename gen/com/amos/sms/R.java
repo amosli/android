da04078fdb49b8f2c9dbc4aa23dc5c0a6e66b1e8
liyayun <amosli@infomorrow.com>
2014-02-27 02:35:42 +0800

    开发短信发送器

--- gen/com/amos/sms/R.java
+++ gen/com/amos/sms/R.java
@@ -40,6 +40,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int error_message=0x7f050005;
         public static final int input_phone=0x7f050002;
         public static final int input_sms=0x7f050003;
         public static final int send=0x7f050004;
--- src/com/amos/sms/MainActivity.java
+++ src/com/amos/sms/MainActivity.java
@@ -2,43 +2,50 @@ package com.amos.sms;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	private EditText et_phone;//文本输入框中的手机号码
-	private EditText et_content;//文本输入框中的短信内容
-	
-	
+	private EditText et_phone;// 文本输入框中的手机号码
+	private EditText et_content;// 文本输入框中的短信内容
+	private String tag = "MainActivity";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		et_phone=(EditText) this.findViewById(R.id.et_phone);
-		et_content=(EditText) this.findViewById(R.id.et_content);
-		
-	
-	
+		et_phone = (EditText) this.findViewById(R.id.et_phone);
+		et_content = (EditText) this.findViewById(R.id.et_content);
+
 	}
 
-	public void send(View v){
+	public void send_sms(View v) {
 		String phone = et_phone.getText().toString();
 		String content = et_content.getText().toString();
-		
-		if("".equals(phone)||"".equals(content)){
-			//提示用户
-			
+		if ("".equals(phone) || "".equals(content)) {
+			Log.e(tag, "手机号或者短信内容为空!" + " phone:" + phone + "  content:" + content);
+			// 提示用户输入手机号码和短信内容
+			Toast.makeText(this, R.string.error_message, Toast.LENGTH_LONG).show();
+			return;
+		} else {
+			System.out.println("phone:" + phone + "  content:" + content);
+			SmsManager smsManger = SmsManager.getDefault();
+			// sentIntent, deliveryIntent
+			// 延期的意图,这里sentIntent是发送报告,运营商返回的信息短信的发送报告等;deliveryIntent表示短信的返回报告
+			Log.w(tag, "点击发送按钮了");
+			// smsManger.sendTextMessage(phone, null, content, null, null);
+			for (String message : smsManger.divideMessage(content)) {
+				smsManger.sendTextMessage(phone, null, message, null, null);
+			}
 		}
-		
-		
 	}
-	
-	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
 }
