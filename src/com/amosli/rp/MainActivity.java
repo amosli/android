76ee4d8e887d04c34969483fb69da195f1b004c0
liyayun <amosli@juxinli.com>
2015-03-18 09:29:48 +0800

    add exit

--- src/com/amosli/rp/MainActivity.java
+++ src/com/amosli/rp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -29,4 +30,19 @@ public class MainActivity extends ActionBarActivity {
 		startActivity(intent);
 	}
 
+	private long exitTime = 0;
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+	    if(keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN){   
+	        if((System.currentTimeMillis()-exitTime) > 2000){  
+	            Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_SHORT).show();                                
+	            exitTime = System.currentTimeMillis();   
+	        } else {
+	            finish();
+	            System.exit(0);
+	        }
+	        return true;   
+	    }
+	    return super.onKeyDown(keyCode, event);
+	}
 }
--- src/com/amosli/rp/ResultActivity.java
+++ src/com/amosli/rp/ResultActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class ResultActivity extends ActionBarActivity {
 
@@ -20,7 +19,7 @@ public class ResultActivity extends ActionBarActivity {
 		String name = intent.getStringExtra("name");
 		int rp = Calculator.calculateRPByName(name);
 		TextView tv_result = (TextView) findViewById(R.id.tv_result);
-		tv_result.setText(name + ",您的人品值为：" + rp);
+		tv_result.setText(name + ",您今天的人品值为：" + rp+"\r\n"+Calculator.getComment(rp));
 		ProgressBar pg = (ProgressBar) findViewById(R.id.pgRP);
 		pg.setProgress(rp);
 	}
@@ -28,16 +27,5 @@ public class ResultActivity extends ActionBarActivity {
 	public void testAgain(View v) {
 		Intent intent = new Intent(this, MainActivity.class);
 		startActivity(intent);
-	}
-
-	public void exitApp(View v) {
-		Toast.makeText(this, "正在退出。。。", Toast.LENGTH_SHORT).show();
-		android.os.Process.killProcess(android.os.Process.myPid());// 获取PID
-		// System.exit(0); // 常规java、c#的标准退出法，返回值为0代表正常退出
-		Intent home = new Intent(Intent.ACTION_MAIN);
-		home.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		home.addCategory(Intent.CATEGORY_HOME);
-		startActivity(home);
-		Toast.makeText(this, "退出成功。。。", Toast.LENGTH_SHORT).show();
 	}
 }
